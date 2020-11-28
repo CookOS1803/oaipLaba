cd8087c8009d430626a8f5345c307633f318fbc5
CookOS <usovich5321@gmail.com>
2020-11-28 20:16:40 +0300

    popravil

--- 6_3/6_3.cpp
+++ 6_3/6_3.cpp
@@ -5,15 +5,25 @@ const int STR_LEN = 256;
 
 int main()
 {
-    char str[STR_LEN],
-         s1[STR_LEN],
-         s2[STR_LEN];
+    char str[STR_LEN];
     int k = 0;
 
     cout << "Enter string: ";
     cin.getline(str, STR_LEN);
 
-    
+    for (int i = 0, j = 1; str[i] != '\0' and j <= 4; i++)
+    {
+        if (j >= 2)
+        {
+            str[k] = str[i];
+            k++;
+        }
+
+        if (str[i] == ' ') j++;
+    }
+    str[k] = '\0';
+
+    cout << str;
 
     system("pause");
 
