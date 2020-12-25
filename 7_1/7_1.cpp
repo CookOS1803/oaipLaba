2e415a3ff4f7e55b6f009d2bb0c130f46d90c224
CookOS <usovich5321@gmail.com>
2020-12-25 15:27:47 +0300

    AAAAA

--- 7_1/7_1.cpp
+++ 7_1/7_1.cpp
@@ -1,12 +1,51 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
 using namespace std;
+
+const int STR_LEN = 256;
+const char* FILE_NAME = "rating.txt";
 
 int main()
 {
-    
+	FILE *f;
+	int choice;
 
+	cout << "1. Create\n2. View\n3. Add\n4. Solve Task\n";
+	cin >> choice;
 
-    system("pause");
+	switch (choice)
+	{
+		case 1:
+			if (!(f = fopen(FILE_NAME, "wt")))
+				cout << "Error\n";
+			else 
+				fclose(f);
+		break;
+		case 2: ;   break;
+		case 3:
+		{
+			if (!(f = fopen(FILE_NAME, "at")))
+			{
+				cout << "Error\n";
+				break;
+			}
 
-    return 0;
+			char str[STR_LEN];
+
+			cout << "Enter last name: ";
+			cin.getline(str, STR_LEN);
+			fwrite(str, sizeof(char), sizeof(str), f);
+
+			fclose(f);
+
+			break;
+		}
+		case 4: ;  break;
+	}
+
+	
+
+	system("pause");
+
+	return 0;
 }
--- 8_3/8_3.cpp
+++ 8_3/8_3.cpp
@@ -1,12 +1,12 @@
 ﻿#include <iostream>
 using namespace std;
 
-double y_normal(double x);
-double y_recursive(double x);
+double y_normal(double x, int n);
+double y_recursive(double x, int n);
 
 int main()
 {
-    double a, b, h;
+    double a, b, h, x;
     int n;
 
     cout << "Enter a, b and h: ";
@@ -15,19 +15,62 @@ int main()
     cout << "Enter n: ";
     cin >> n;
 
-    n = n % 2 ? n - 1 : n;
-    double x = a;
+    x = a;
 
     while (x <= b)
     {
-        
-
-        cout << "Y(x) normal: "    << y_normal(x)    << endl
-             << "Y(x) recursive: " << y_recursive(x) << endl
+        cout << "Y(" << x << ") normal: "    << y_normal(x, n)    << endl
+             << "Y(" << x << ") recursive: " << y_recursive(x, n) << endl
              << endl;
+        x += h;
     }
 
     system("pause");
 
     return 0;
+}
+
+double y_normal(double x, int n)
+{    
+    if (n == n % 2)
+    {
+        return -1;
+    }
+    else
+    {
+        if (n == 0) return 1;
+
+        double y;
+
+        if (n > 0)
+        {
+            y = x;
+
+            while (n > 1)
+            {
+                y *= x;
+                n--;
+            }
+        }
+        else
+        {
+            y = 1/x;
+
+            while (n < -1)
+            {
+                y *= 1/x;
+                n++;
+            }
+        }
+
+        return y;
+    }
+}
+
+double y_recursive(double x, int n)
+{
+    if (n == 0) return 1;
+
+    if (n > 0) return y_recursive(x, --n) * x;
+    else       return y_recursive(x, ++n) * 1/x;
 }
