63e8501e9a94ce62b8d79fe50e03d77978b0688d
usovich5321 <usovich5321@gmail.com>
2021-02-12 11:49:28 +0300

    i

--- 8_3/8_3.cpp
+++ 8_3/8_3.cpp
@@ -1,13 +1,8 @@
 ﻿#include <iostream>
 using namespace std;
 
-const int ITER_COUNT = 5;
-
-double power(double x, int n);
 double y_normal(double x, int n);
 double y_recursive(double x, int n);
-double y_r_even(double x, int n);
-double y_r_odd(double x, int n, double guess = 1, int k = ITER_COUNT);
 
 int main()
 {
@@ -35,8 +30,9 @@ int main()
     return 0;
 }
 
-double power(double x, int n)
+double y_normal(double x, int n)
 {
+    if (n % 1) n--;
     if (n == 0) return 1;
 
     double y;
@@ -65,60 +61,16 @@ double power(double x, int n)
     return y;
 }
 
-double y_normal(double x, int n)
-{    
-    if (n % 2)
-    {
-        n = 2 - n;
-        if (n == 1) return x;
-        if (n == -1) return 1/x;
-        
-        double y = 1;
-        bool neg = false;
-        if (n < 0)
-        {
-            n = -n;
-            neg = true;
-        }
-
-        for (int i = 0; i < ITER_COUNT; i++)
-        {
-            y = y*(1 - (1 - x/power(y, n))/n);
-        }
-
-        return neg ? 1/y : y;
-    }
-    else
-    {
-        return power(x, n);
-    }
-}
-
 double y_recursive(double x, int n)
 {
     if (n % 2)
     {
-        if (2 - n == 1) return x;
-        if (2 - n == -1) return 1/x;
-        return y_r_odd(x, 2 - n);
+        //if ()
+
     }
-    else return y_r_even(x, n);
-}
-
-double y_r_even(double x, int n)
-{
-    if (n == 0) return 1;
-
-    if (n > 0) return y_r_even(x, --n) * x;
-    else       return y_r_even(x, ++n) * 1/x;
-}
-
-double y_r_odd(double x, int n, double guess, int k)
-{
-    if (k < 1) return guess;
-
-    if (n > 0)
-        return y_r_odd(x, n, guess*(1 - (1 - x/power(guess, n))/n), --k);
     else
-        return 1/y_r_odd(x, -n, guess*(1 - (1 - x/power(guess, -n))/(-n)), --k);
+    {
+        if (n == 0) return 1;
+
+    }
 }
