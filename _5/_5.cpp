3ecb46ab9cf2bd5029df97b2b5971fc7484415bc
CookOS <usovich5321@gmail.com>
2021-05-05 00:19:37 +0300

    http

--- _5/_5.cpp
+++ _5/_5.cpp
@@ -3,52 +3,62 @@
 
 using namespace std;
 
-struct stack_c
+template <typename T> struct stack
 {
-    char info;
-    stack_c* next = nullptr;
+    T info;
+    stack* next = nullptr;
 };
 
-struct stack_d
-{
-    double info;
-    stack_d* next = nullptr;
-};
-
-stack_c* in_stack(stack_c* s, char info);
-stack_c* out_stack(stack_c* s, char* info);
-stack_d* in_stack(stack_d* s, double info);
-stack_d* out_stack(stack_d* s, double* info);
-char* translate(char* in);
-double result(char* str, double* data);
+template <typename T> stack<T>* in_stack(stack<T>* s, T info);
+template <typename T> stack<T>* out_stack(stack<T>* s, T* info);
+template <typename T> stack<T>* out_stack(stack<T>* s);
+char* translate(const char* in, char* out);
+double result(const char* str, double* data);
 int priority(char c);
+int check(const char* str);
 
 int main()
 {
-    char str[32];// = "(a-b)/(c+d)*e";
-    double data[5];
+    char str[32],       // = "(a-b)/(c+d)*e";
+         rpn[32] = "";
+    double data[26];
+    bool input = true;
 
-    cout << "Enter formula: ";
-    cin.getline(str, sizeof(str));
+    while (input)
+    {
+        cout << "Enter formula: ";
+        cin.getline(str, sizeof(str));
 
-    strcpy(str, translate(str));
-    cout << str << endl;
+        switch (check(str))
+        {
+            case 1: cout << "Missing brackets\n"; break;
+            case 2: cout << "Missing operator\n"; break;
+            case 3: cout << "Missing operand\n";  break;
+
+            default: input = false;
+        }
+    }
+
+    cout << translate(str, rpn) << endl;
 
     for (int i = 0; i < sizeof(data) / sizeof(data[0]); i++)
     {
-        cout << "Enter " << (char)(i + 97) << ": ";
-        cin >> data[i];
+        if (strchr(rpn, i + 97))
+        {
+            cout << "Enter " << (char)(i + 97) << ": ";
+            cin >> data[i];
+        }
     }
-    cout << endl;
-    cout << result(str, data) << endl;
+
+    cout << endl << result(rpn, data) << endl;
 
     system("pause");
     return 0;
 }
 
-stack_c* in_stack(stack_c* s, char info)
+template <typename T> stack<T>* in_stack(stack<T>* s, T info)
 {
-    stack_c* t = new stack_c;
+    stack<T>* t = new stack<T>;
 
     t->info = info;
     t->next = s;
@@ -56,44 +66,32 @@ stack_c* in_stack(stack_c* s, char info)
     return t;
 }
 
-stack_c* out_stack(stack_c* s, char* info)
+template <typename T> stack<T>* out_stack(stack<T>* s, T* info)
 {
-    stack_c* t = s;
+    stack<T>* t = s;
 
-    if (info) *info = s->info;
+    *info = s->info;
     s = s->next;
 
     delete t;
     return s;
 }
 
-stack_d* in_stack(stack_d* s, double info)
+template <typename T> stack<T>* out_stack(stack<T>* s)
 {
-    stack_d* t = new stack_d;
+    stack<T>* t = s;
 
-    t->info = info;
-    t->next = s;
-
-    return t;
-}
-
-stack_d* out_stack(stack_d* s, double* info)
-{
-    stack_d* t = s;
-
-    if (info) *info = s->info;
     s = s->next;
 
     delete t;
     return s;
 }
 
-char* translate(char* in)
+char* translate(const char* in, char* out)
 {
-    stack_c *s = nullptr;
+    stack<char> *s = nullptr;
     char c,
-         a[2],
-         out[32] = "";
+         a[2];
 
     a[1] = '\0';
 
@@ -117,12 +115,11 @@ char* translate(char* in)
                     s = out_stack(s, a);
                     strcat(out, a);
                 }
-                s = out_stack(s, nullptr);
+                s = out_stack(s);
             break;
-            case '*': case '/': case '+': case '-':
+            case '*': case '/': case '+': case '-': case '^':
                 while (s and priority(s->info) >= priority(c))
                 {
-
                     s = out_stack(s, a);
                     strcat(out, a);
                 }
@@ -136,14 +133,13 @@ char* translate(char* in)
         s = out_stack(s, a);
         strcat(out, a);
     }
-    
+
     return out;
 }
 
-double result(char* str, double* data)
+double result(const char* str, double* data)
 {
-    stack_d *d = nullptr;
-    char t;
+    stack<double> *d = nullptr;
     double op1, op2, res;
 
     for (int i = 0; i < strlen(str); i++)
@@ -156,10 +152,11 @@ double result(char* str, double* data)
 
             switch (str[i])
             {
-                case '+': res = op2 + op1; break;
-                case '-': res = op2 - op1; break;
-                case '*': res = op2 * op1; break;
-                case '/': res = op2 / op1; break;
+                case '+': res = op2 + op1;     break;
+                case '-': res = op2 - op1;     break;
+                case '*': res = op2 * op1;     break;
+                case '/': res = op2 / op1;     break;
+                case '^': res = pow(op2, op1); break;
             }
             
             d = in_stack(d, res);
@@ -174,9 +171,60 @@ int priority(char c)
 {
     switch (c)
     {
-        case '*': case '/': return 3;
-        case '+': case '-': return 2;
-        case '(':           return 1;
+        case '^':           return 3;
+        case '*': case '/': return 2;
+        case '+': case '-': return 1;
         default:            return 0;
     }
+}
+
+int check(const char* str)
+{
+    int len     = strlen(str),
+        open    = 0,
+        closing = 0,
+        i       = 0;
+    char c, c1, c2;
+
+    while (i < len)
+    {
+
+        //if (c < 'a' or c > 'z' and !priority(c)) continue;
+
+        if      (str[i] == '(') open++;
+        else if (str[i] == ')') closing++;
+
+        i++;
+    }
+    if (open != closing) return 1;
+
+    i = 0;
+
+    while (i < len and ((str[i] < 'a' or str[i] > 'z') and !priority(str[i]) and str[i] != '(')) i++;
+    c1 = str[i];
+    if (priority(c1)) return 3;
+    i++;
+
+    while (i < len)
+    {
+        c = str[i];
+
+        if ((c < 'a' or c > 'z') and !priority(c) and c != ')' and c != '(')
+        {
+            i++;
+            continue;
+        }
+
+        if ((priority(c1) or c1 == '(') and priority(c)) return 3;
+        if (!priority(c1) and c1 != '(' and !priority(c) and c != '(' and c != ')') return 2;
+
+        c1 = c;
+
+        i++;
+    }
+
+    if (priority(c1)) return 3;
+
+
+    return 0;
 }
